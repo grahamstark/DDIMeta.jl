fed13e838ff2ec24cb7cc67801ec80a1ae436a00
Graham Stark <graham.stark@virtual-worlds.biz>
2018-11-06 16:59:31 +0000

    Dependency on IniFile corrected.

--- src/DDIMeta.jl
+++ src/DDIMeta.jl
@@ -4,18 +4,21 @@ module DDIMeta.jl
     using DataStructures
     using DataFrames
     using LightXML
+    using IniFile
 
     # jdbc dao stuff for our UKDS meta database
-    export getconnection, loadvariablelist, init, shutdown
+    export getconnection, loadvariablelist, init, shutdown, DBInfo
+
     # XML utilities
     export basiccensor, getIntAttr, getStrContent, getIntContent
     export getIntContentAttr
-    # DDI XML parsing
-    export parseddi
+
     # meta stuff
     export EnumVal, EnumsDict, Variable, VariableList
     export adddummies!, isprobablymissing
 
+    # DDI XML parsing
+    export parseddi
 
     include( "meta.jl" )
     include( "xmlutils.jl")
